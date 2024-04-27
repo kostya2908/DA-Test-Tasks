89078ce0da852510f044d74f9075ebf42ae26a08
Konstantin Mokievets <k.mokievets@gmail.com>
2024-04-28 01:26:30 +0300

    part 3 - reverse list

---
+++ 05 - Amazon_SQL_Python/09_revers_list.py
@@ -0,0 +1,27 @@
+a = [1,2,3,4,5,6,7,8,9]
+
+#FIRST:
+
+first = a[::-1]
+print(f'first:\n{first}')
+
+#SECOND:
+
+left = 0
+right = len(a) - 1
+second = a.copy()
+while left < right:
+    temp = second[left]
+    second[left] = second[right]
+    second[right] = temp
+    left += 1
+    right -= 1
+print(f'second:\n{second}')
+
+#THIRD:
+
+third = []
+for i in a:
+    third.insert(0, i)
+print(f'third:\n{third}')
+
